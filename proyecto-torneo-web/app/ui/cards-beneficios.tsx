6d22caca2c23d090a015f9c832a67239b91ec1ff
Marisol Quevedo <marisolfqd@gmail.com>
2026-06-04 15:29:42 -0500

    add images as icons in benefits

--- proyecto-torneo-web/app/ui/cards-beneficios.tsx
+++ proyecto-torneo-web/app/ui/cards-beneficios.tsx
@@ -3,17 +3,19 @@ import Image from "next/image";
 interface BeneficiosProps{
   titulo:string,
   descripcion:string,
-  icono:string
+  icono:string,
 }
-export default function CardsBeneficios({titulo, descripcion, icono}:BeneficiosProps) {
+export default function CardsBeneficios({titulo, descripcion, icono,}:BeneficiosProps) {
   return (
     <div className="card-beneficios">
-      <p>{icono}</p>
-      {/* Idaelmente serián imágenes, se tendría que cambiar en el data el ícono por una url o algo así
-      <Image src=''
+      
+      
+      <Image src={icono}
         alt={titulo}
-        height={40}/>    
-      */}
+        height={40}
+        width={40}
+        />    
+      
       <h3 className="subtitulo-beneficios fuente-subtitulos ">{titulo}</h3>
       <p className="descripcion-beneficios fuente-parrafos">{descripcion}</p>
     </div>
